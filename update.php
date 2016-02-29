b66bac467ed7e9b1af167602ef6743d8363c83b9
Антон Кияткин <a.kiyatkin@itlf.ru>
2016-02-29 15:36:14 +0400

    update event

--- update.php
+++ update.php
@@ -2,11 +2,15 @@
 	use infrajs\load\Load;
 	use infrajs\access\Access;
 	use infrajs\event\Event;
+	use infrajs\config\Config;
 	
-	$ans = Load::loadJSON('-tester/?type=auto');
-	if (!$ans || !$ans['result']) {
-		error_log('TESTER: There are errors in the tests /-tester/?type=errors '.$ans['msg']);
-		if (Access::debug()) die('TESTER: There are errors in the tests <a href="/-tester/?type=errors">errors</a> '.$ans['msg']);
+	$conf=Config::get('tester');
+	if ($conf['updatetest']) {
+		$ans = Load::loadJSON('-tester/?type=auto');
+		if (!$ans || !$ans['result']) {
+			error_log('TESTER: There are errors in the tests /-tester/?type=errors '.$ans['msg']);
+			if (Access::debug()) die('TESTER: There are errors in the tests <a href="/-tester/?type=errors">errors</a> '.$ans['msg']);
+		}
 	}
 
 ?>
