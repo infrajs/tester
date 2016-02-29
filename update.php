d8da9c3c0a68397e2180298e3396b817a3c4ff9d
Антон Кияткин <a.kiyatkin@itlf.ru>
2016-02-29 18:43:35 +0400

    update with tests

--- update.php
+++ update.php
@@ -3,13 +3,15 @@
 	use infrajs\access\Access;
 	use infrajs\event\Event;
 	use infrajs\config\Config;
+	use infrajs\update\Update;
 	
 	$conf=Config::get('tester');
 	if ($conf['updatetest']) {
+		Update::exec(); //Это отодвинет update testerа в конец очереди
 		$ans = Load::loadJSON('-tester/?type=auto');
 		if (!$ans || !$ans['result']) {
 			error_log('TESTER: There are errors in the tests /-tester/?type=errors '.$ans['msg']);
-			if (Access::debug()) die('TESTER: There are errors in the tests <a href="/-tester/?type=errors">errors</a> '.$ans['msg']);
+			if (Access::debug()) die('TESTER: There are errors in the tests <a href="/-tester/?type=errors">errors</a> <b>'.$ans['msg'].'</b>');
 		}
 	}
 
