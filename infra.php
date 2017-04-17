dc9defff2fd76c909bba4f0697c991ad9327e338
Anton <a.kiyatkin@itlf.ru>
2017-04-17 23:04:02 +0400

    fix

--- infra.php
+++ infra.php
@@ -6,22 +6,19 @@ use infrajs\access\Access;
 use infrajs\load\Load;
 use infrajs\nostore\Nostore;
 
-if (!is_file('vendor/autoload.php')) {
-	chdir('../../../');
-	require_once('vendor/autoload.php');
-	Router::init();
-}
-
 $action = Ans::GET('-tester');
+
 if ($action == 'true') {
 	Nostore::on();
+
 	if (Access::test()) {
 		$ans = Load::loadJSON('-tester/?type=auto');
 		if (!$ans || !$ans['result']) {
-			error_log('TESTER: There are errors in the tests /-tester/?type=errors '.$ans['msg']);
+			echo '<div class="alert alert-danger"><a href="/-tester/?type=errors">Тестирование</a> выполнено с ошибками.</div>';
+			//error_log('TESTER: There are errors in the tests /-tester/?type=errors '.$ans['msg']);
 			//if (Access::debug()) die('TESTER: There are errors in the tests <a href="/-tester/?type=errors">errors</a> <b>'.$ans['msg'].'</b>');
 		} else {
-			echo '<div class="well"><a href="/-tester/">Тестирование</a> выполнено, ошибок не обнаружено.</div>';
+			echo '<div class="alert alert-success"><a href="/-tester/">Тестирование</a> выполнено, ошибок не обнаружено.</div>';
 		}
 	} else {
 		$ans = array();
