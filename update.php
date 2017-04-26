aabb2452e2f785e9a3ca9672d941b0715e910ac7
Anton <a.kiyatkin@itlf.ru>
2017-04-26 18:36:16 +0400

    msg always

--- update.php
+++ update.php
@@ -6,10 +6,11 @@
 	use infrajs\update\Update;
 	
 	$conf = Config::get('tester');
-	if ($conf['updatetest']) {
+	if ($conf['updatetest'] && Access::debug()) {
 		Update::exec(); //Это отодвинет update testerа в конец очереди
 		$ans = Load::loadJSON('-tester/?type=auto');
 		if (!$ans || !$ans['result']) {
+			echo '<div class="alert alert-danger"><a href="/-tester/?type=errors">Тестирование</a> выполнено с ошибками.</div>';
 			error_log('TESTER: There are errors in the tests /-tester/?type=errors '.$ans['msg']);
 			//if (Access::debug()) die('TESTER: There are errors in the tests <a href="/-tester/?type=errors">errors</a> <b>'.$ans['msg'].'</b>');
 		} else {
